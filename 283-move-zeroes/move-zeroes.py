5ca3f09a65a4c6311e5564254f1c54c988c080ec
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2024-07-16 14:13:12 -0400

    Time: 129 ms (44.34%) | Memory: 18.1 MB (15.94%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,29 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        zerocount= nums.count(0)
+        i =0
+        while i < len(nums):
+            if nums[i]==0:
+                nums.pop(i)
+
+            else:
+                i=i+1
+
+        nums.extend([0]*zerocount)
+
+            
+                
+            
+    
+
+            
+           
+            
+                
+
+
+        
