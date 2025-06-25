e3d2450462e515f4aecbbd7d03aba536e233d16f
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-06-25 16:50:11 -0400

    Time: 2 ms (49.94%) | Memory: 41.7 MB (51.32%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack1 = new Stack<>();
+
+        for (char c : s.toCharArray()){
+            if(c=='#'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+            else {
+                stack.push(c);
+            }
+
+        }
+
+        for (char c : t.toCharArray()){
+             if(c=='#'){
+                if(!stack1.isEmpty()){
+                    stack1.pop();
+                }
+            }
+            else {
+                stack1.push(c);
+            }
+            
+        }
+
+        if(stack.equals(stack1)){
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+}
