5a77ea15cd9c81283070449a2814b832e3829276
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-06-30 17:02:00 -0400

    Time: 2 ms (94.69%) | Memory: 42.4 MB (50.25%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+        boolean[] presence = new boolean[26];
+
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+
+        Stack<Character> stack1 = new Stack<>();
+
+        for(char c: s.toCharArray()){
+            freq[c-'a']--;
+
+
+            if(presence[c-'a']==true){
+                continue;
+            }
+            while(!stack1.isEmpty() && freq[stack1.peek()-'a']>0 && stack1.peek()>c){
+                presence[stack1.peek()-'a']=false;
+                stack1.pop();
+
+            }
+
+            stack1.push(c);
+            presence[c-'a']=true;
+        }
+
+        StringBuilder a = new StringBuilder();
+
+        while(!stack1.isEmpty()){
+            a.insert(0,stack1.pop());
+        }
+        
+        return a.toString();
+    }
+}
