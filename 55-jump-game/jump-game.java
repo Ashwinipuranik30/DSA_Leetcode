f1627e3a0240107b1e1626b744c074227b2917c4
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-08-14 14:42:52 -0400

    Time: 3 ms (48.23%) | Memory: 46 MB (31.49%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+      
+        int index=0;
+        int max=0;
+        
+        while(index<(nums.length-1)){
+            if(max<index){
+                break;
+            }
+            max=Math.max(max,index+nums[index]);
+            index++;
+        }
+        if(max>=(nums.length-1)){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
