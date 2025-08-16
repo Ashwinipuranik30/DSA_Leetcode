1bce77d48276e89ba6e5bd217f783039b1644489
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-08-16 10:24:33 -0400

    Time: 3 ms (56.10%) | Memory: 56.2 MB (69.11%) - LeetSync

---
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long numberOfWeeks(int[] milestones) {
+        long max=milestones[0];
+        long sum=0;
+        for(int i=0;i<milestones.length;i++){
+            max=Math.max(max,milestones[i]);
+            sum+=milestones[i];
+        }
+
+        sum=sum-max;
+
+        if(milestones.length==1){
+            return 1;
+        }
+
+        else if(max<=sum+1){
+            return max+sum;
+        }
+        else{
+            return sum*2+1;
+        }
+        
+    }
+}
