e1f96b67f0e28a9df843a81908777edda216d93b
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-08-21 12:44:26 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (46.04%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    private int rob_mem(int index ,int[] map, int[] nums){
+        if(index>=map.length){
+            return 0;
+        }
+        if(map[index]!=-1){
+            return map[index];
+        }
+        
+        int a=rob_mem(index+2,map,nums)+ nums[index];
+        int b=rob_mem(index+1,map,nums);
+
+        map[index]=Math.max(a,b);
+        return map[index];
+
+
+    }
+    public int rob(int[] nums) {
+        int[] mem= new int[nums.length];
+        Arrays.fill(mem,-1);
+        return rob_mem(0,mem,nums);
+        
+    }
+}
