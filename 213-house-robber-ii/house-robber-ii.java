5ffd25da059919fc08f34e33e38f66bb6b0e642b
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-08-21 16:03:16 -0400

    Time: 0 ms (100.00%) | Memory: 40.9 MB (91.37%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,6 +1,20 @@
 class Solution {
+
+    private int rob_bottomup(int[] nums,int[] map,int index){
+        if(index>=map.length){
+            return 0;
+        }
+        if(map[index]!=-1){
+            return map[index];
+        }
+        int a= rob_bottomup(nums,map,index+2) + nums[index%nums.length];
+        int b=rob_bottomup(nums,map,index+1);
+        map[index]=Math.max(a,b);
+        return map[index];
+
+    }
     public int rob(int[] nums) {
-        int a=0;
+        /*int a=0;
         int b=nums[0];
         if(nums.length==1){
             return nums[0];
@@ -20,9 +34,13 @@ class Solution {
             u=g;
         }
         
-        return b-u;
+        return b-u;*/
 
-
+        int[] memo = new int[nums.length];
+        int [] mem = new int[nums.length*2];
+        Arrays.fill(memo,-1);
+        Arrays.fill(mem,-1);
+        return nums.length==1 ? nums[0] : rob_bottomup(nums,mem,0)-rob_bottomup(nums,memo,0);
         
     }
 }
