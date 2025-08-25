eeead13648764c2257bd79d1d6ffab85f4846d0a
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-08-25 13:53:20 -0400

    Time: 14 ms (42.60%) | Memory: 42.2 MB (38.18%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    private int numRolls(int dice, int face, int target,int[][] dp){
+    /*private int numRolls(int dice, int face, int target,int[][] dp){
         if(dice==0 && target==0){
             return 1;
         }
@@ -18,13 +18,29 @@ class Solution {
         return ways;
 
     }
+    */
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n+1][target+1];
-        for(int i=0;i<=n;i++){
+        /*for(int i=0;i<=n;i++){
             Arrays.fill(dp[i],-1);
         }
         return numRolls(n ,k ,target, dp);
-        
+        */
+
+        dp[0][0]=1;
+        for(int dice =1;dice<=n;dice++){
+            for(int tar=1;tar<=target;tar++){
+                int ways=0;
+                for(int face=1;face<=k;face++){
+                    if(tar>=face){
+                        ways=ways+dp[dice-1][tar-face];
+                        ways=ways%1000_000_007;
+                    }
+                }
+                dp[dice][tar]=ways;
+            }
+        }
+        return dp[n][target];
         
     }
 }
