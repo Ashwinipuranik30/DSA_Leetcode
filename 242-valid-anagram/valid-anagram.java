04ac0cae3f360b9c5faaab93498c0beb56c305e7
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-08-27 22:26:40 -0400

    Time: 4 ms (77.16%) | Memory: 43.2 MB (76.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,7 +3,7 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        Map<Character,Integer> map1 = new HashMap<>();
+        /* Map<Character,Integer> map1 = new HashMap<>();
 
         for(int i=0;i<s.length();i++){
             map1.put( s.charAt(i) , map1.getOrDefault(s.charAt(i),0)+1 );
@@ -24,6 +24,26 @@ class Solution {
             }
         }
         return true;
+        */
+
+        int[] count = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)-'a']--;
+        }
+
+        for(int c: count){
+            if(c!=0){
+                return false;
+            }
+        }
+        return true;
+
+
 
 
 
