7fdce91885a70117e378bb6488c9bdaa2b54eda6
Ashwini Puranik <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-10-20 17:19:35 -0400

    Time: 8 ms (55.04%) | Memory: 42.7 MB (81.28%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
+        if(s2.length()<s1.length()){
+            return false;
+        }
+
         int[] freqs1 = new int[26];
+        int[] count2 = new int[26];
 
         for(int i=0; i< s1.length(); i++){
             freqs1[s1.charAt(i)-'a']++;
+            count2[s2.charAt(i)- 'a']++;
+
         }
 
 
-        for(int i=0;i<=(s2.length()-s1.length()) ; i++){
-            int end=i+s1.length()-1;
-            int[] count2 = new int[26];
-            for(int j=i ; j<=end ;j++){
-                count2[s2.charAt(j)-'a']++;
-            }
-
+        for(int i=s1.length(); i<s2.length(); i++){
             if(Arrays.equals(freqs1,count2)){
                 return true;
             }
 
+
+            count2[s2.charAt(i-s1.length())-'a']--;
+            count2[s2.charAt(i)-'a']++;
         }
 
-        return false;
+        return Arrays.equals(count2,freqs1);
 
 
         
