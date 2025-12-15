928882b1ec85310a37c3fa0e78061c62ff5609d6
Ashwini Puranik <144976846+Ashwinipuranik30@users.noreply.github.com>
2025-12-15 15:47:17 -0500

    Time: 544 ms (13.13%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-SELECT B.id FROM Weather A , Weather B 
-WHERE A.temperature<B.temperature AND DATEDIFF(B.recordDATE,A.recordDate)=1;
+#SELECT B.id FROM Weather A , Weather B 
+#WHERE A.temperature<B.temperature AND DATEDIFF(B.recordDATE,A.recordDate)=1;
+
+SELECT B.id FROM Weather A JOIN Weather B ON A.temperature<B.temperature AND 
+DATEDIFF(B.recordDATE,A.recordDate)=1;
