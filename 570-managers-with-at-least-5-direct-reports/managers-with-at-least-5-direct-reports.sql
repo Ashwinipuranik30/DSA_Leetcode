3ed095c5107ce4b6821234e81b4c706f550a609e
Ashwini Puranik <144976846+Ashwinipuranik30@users.noreply.github.com>
2026-07-30 16:21:25 -0400

    Time: 397 ms (25.41%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,6 @@
-SELECT name FROM Employee 
-WHERE id IN (SELECT managerId FROM Employee GROUP BY managerID Having Count(managerId)>=5);
+#SELECT name FROM Employee 
+#WHERE id IN (SELECT managerId FROM Employee GROUP BY managerID Having Count(managerId)>=5);
+
+
+SELECT E2.name FROM EMPLOYEE E 
+JOIN Employee E2 ON E.managerId=E2.id GROUP BY E.managerId Having COUNT(E.managerId)>=5;
