8451eec4fa780e8575ba51d29500bf101e7b8866
Ashwini Puranik <144976846+Ashwinipuranik30@users.noreply.github.com>
2026-08-04 11:44:34 -0400

    Time: 1063 ms (12.87%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
-DELETE P FROM Person P , Person P1 where P.email=P1.email AND P.id>P1.id;
+DELETE P from Person P, Person P1 WHERE P.email=P1.email AND P.id>P1.id;
